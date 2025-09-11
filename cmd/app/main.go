7a10122ea5130c3240c8e60ad96229b3b29f6671
Lucas703F <lucasfelipe.araujo0@gmail.com>
2025-09-11 20:46:09 -0300

    Desafio 1 - Alteração da mensagem

---
+++ cmd/app/main.go
@@ -0,0 +1,16 @@
+// Arquivo principal do programa (entrypoint) 🫡
+// Convenção de mercado: colocar em cmd/<nome-app>/main.go
+package main
+
+// Importa os pacotes necessários
+import (
+	"fmt"
+	"github.com/seu-usuario/meu-projeto-go/internal/hello"
+)
+
+// Função principal do programa
+func main() {
+	// Mensagem inicial da aplicação
+	fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
+	hello.SayHello()
+}
